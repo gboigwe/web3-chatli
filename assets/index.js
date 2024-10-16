8a456cb2ada4e376ff1222fd745682706bb9b8c7
Gbolahan Akande <geakande@gmail.com>
2024-10-16 17:19:27 +0100

    Commiting Chat Application

---
+++ assets/index.js
@@ -0,0 +1,65 @@
+import LOGO from "./logo.png";
+import CLOSE from "./close.png";
+import OPEN from "./open.png";
+import SEARCH from "./search.png";
+import AVATAR from "./avatar.gif";
+import SMILE from "./smile.png";
+import SEND from "./send.png";
+import FILE from "./file.png";
+import USER from "./user.png";
+import CLEAR from "./delete.png";
+import CREATE from "./create.png";
+import CREATE2 from "./create2.png";
+import BUDDY from "./buddy.png";
+import HERO from "./hero.png";
+import ACCOUNT from "./account.png";
+import USERNAME from "./username.png";
+import FRIENDS from "./friends.png";
+import FRIENDS2 from "./friends2.png";
+import ACCOUNTNAME from "./acountName.png";
+import DOT from "./dot.png";
+import LOADER from "./loader.gif";
+import IMG1 from "./img1.gif";
+import IMG2 from "./img2.gif";
+import IMG3 from "./img3.gif";
+import IMG4 from "./img4.gif";
+import IMG5 from "./img5.gif";
+import IMG6 from "./img6.gif";
+import IMG7 from "./img7.gif";
+import IMG8 from "./img8.gif";
+import IMG9 from "./img9.gif";
+import IMG10 from "./img10.gif";
+
+export default {
+  LOGO,
+  CLOSE,
+  OPEN,
+  SEARCH,
+  AVATAR,
+  SMILE,
+  SEND,
+  FILE,
+  USER,
+  CLEAR,
+  CREATE,
+  CREATE2,
+  BUDDY,
+  HERO,
+  ACCOUNT,
+  USERNAME,
+  FRIENDS,
+  FRIENDS2,
+  ACCOUNTNAME,
+  DOT,
+  LOADER,
+  IMG1,
+  IMG2,
+  IMG3,
+  IMG4,
+  IMG5,
+  IMG6,
+  IMG7,
+  IMG8,
+  IMG9,
+  IMG10,
+};
